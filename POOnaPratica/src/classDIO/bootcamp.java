dcf991327fb7c978add427fb1ecf0866f8c282e6
hellyaxs <eliasvitor.dev@gmail.com>
2022-02-02 22:22:26 -0300

    implementação de equals &hashCode,polimorfismo, Stream API,e reference method

--- POOnaPratica/src/classDIO/bootcamp.java
+++ POOnaPratica/src/classDIO/bootcamp.java
@@ -2,6 +2,8 @@ package classDIO;
 
 import java.time.LocalDate;
 import java.util.HashSet;
+import java.util.LinkedHashSet;
+import java.util.Objects;
 import java.util.Set;
 
 public class bootcamp {
@@ -10,10 +12,59 @@ public class bootcamp {
     private final LocalDate inicio = LocalDate.now();
     private final LocalDate dataFinal = inicio.plusDays(45);
     private String descricao;
-    private Set<dev> devsInscrtos;
+    private Set<dev> devsInscrtos = new HashSet<>();
+    private Set<conteudo> conteudo = new LinkedHashSet<>();
 
-    public void devInscrito(dev dev){
-        devsInscrtos = new HashSet<>();
-        devsInscrtos.add(dev);
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public LocalDate getInicio() {
+        return inicio;
+    }
+
+    public LocalDate getDataFinal() {
+        return dataFinal;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public Set<dev> getDevsInscrtos() {
+        return devsInscrtos;
+    }
+
+    public void setDevsInscrtos(Set<dev> devsInscrtos) {
+        this.devsInscrtos = devsInscrtos;
+    }
+
+    public Set<classDIO.conteudo> getConteudo() {
+        return conteudo;
+    }
+
+    public void setConteudo(Set<classDIO.conteudo> conteudo) {
+        this.conteudo = conteudo;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof bootcamp)) return false;
+        bootcamp bootcamp = (bootcamp) o;
+        return Objects.equals(getNome(), bootcamp.getNome()) && Objects.equals(getInicio(), bootcamp.getInicio()) && Objects.equals(getDataFinal(), bootcamp.getDataFinal()) && Objects.equals(getDescricao(), bootcamp.getDescricao()) && Objects.equals(getDevsInscrtos(), bootcamp.getDevsInscrtos()) && Objects.equals(getConteudo(), bootcamp.getConteudo());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(getNome(), getInicio(), getDataFinal(), getDescricao(), getDevsInscrtos(), getConteudo());
     }
 }
--- POOnaPratica/src/classDIO/dev.java
+++ POOnaPratica/src/classDIO/dev.java
@@ -1,14 +1,67 @@
 package classDIO;
 
-import java.util.List;
+import java.util.*;
 
 public class dev {
     private String nome;
-    private List<conteudo> conteudosInscritos;
-    private List<conteudo> conteudosConcluidos;
+    private Set<conteudo> conteudosInscritos = new LinkedHashSet<>();
+    private Set<conteudo> conteudosConcluidos = new LinkedHashSet<>();
 
 
     public void inscreverBootcamp(bootcamp bootcamp){
+          conteudosInscritos.addAll(bootcamp.getConteudo());
+          bootcamp.getDevsInscrtos().add(this);
+    }
 
+    public void progredir(){
+       Optional<conteudo> first = conteudosInscritos.stream().findFirst();
+       if(first.isPresent()){
+           conteudosConcluidos.add(first.get());
+           conteudosInscritos.remove(first.get());
+       }else{
+           System.out.println("voce nao esta inscritor em nemhum bootcamp");
+       }
+
+    }
+
+    public double calculartotalXp(){
+        return this.conteudosConcluidos.stream().mapToDouble(conteudo::calcularXP).sum();
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public Set<conteudo> getConteudosInscritos() {
+        return conteudosInscritos;
+    }
+
+    public void setConteudosInscritos(Set<conteudo> conteudosInscritos) {
+        this.conteudosInscritos = conteudosInscritos;
+    }
+
+    public Set<conteudo> getConteudosConcluidos() {
+        return conteudosConcluidos;
+    }
+
+    public void setConteudosConcluidos(Set<conteudo> conteudosConcluidos) {
+        this.conteudosConcluidos = conteudosConcluidos;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof dev)) return false;
+        dev dev = (dev) o;
+        return Objects.equals(getNome(), dev.getNome()) && Objects.equals(getConteudosInscritos(), dev.getConteudosInscritos()) && Objects.equals(getConteudosConcluidos(), dev.getConteudosConcluidos());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(getNome(), getConteudosInscritos(), getConteudosConcluidos());
     }
 }
